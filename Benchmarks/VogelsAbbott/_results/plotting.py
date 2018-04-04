d84237770b6e5c428f0eda047400a33962b89418
Nasir Ahmad <nasiryahm@gmail.com>
2018-04-04 14:43:53 +0100

    Changes to plot title etc.

--- Benchmarks/VogelsAbbott/_results/plotting.py
+++ Benchmarks/VogelsAbbott/_results/plotting.py
@@ -29,28 +29,28 @@ for s in simulators:
 
 # Plotting single timestep results
 fig, ax = plt.subplots()
-ax.set_title("Vogels-Abbott Benchmark Result with 0.1ms delay")
+ax.set_title("Vogels-Abbott Benchmark Result with 0.1ms synaptic delays")
 ax.bar(
         range(len(simulators)),
         single_timestep_delay_results,
         tick_label=simulators,
         align='center', color='k')
 ax.set_xlabel("Simulator")
-ax.set_ylabel("Simulation Speed Relative to Real-time")
+ax.set_ylabel("Simulation Speed (Normalized)")
 ax.set_yscale('log')
 fig.savefig('single_delay_comparison.png')
 #plt.show()
 
 
 fig, ax = plt.subplots()
-ax.set_title("Vogels-Abbott Benchmark Result with 0.8ms delay")
+ax.set_title("Vogels-Abbott Benchmark Result with 0.8ms synaptic delays")
 ax.bar(
         range(len(simulators)),
         eight_timestep_delay_results,
         tick_label=simulators,
         align='center', color='k')
 ax.set_xlabel("Simulator")
-ax.set_ylabel("Simulation Speed Relative to Real-time")
+ax.set_ylabel("Simulation Speed (Normalized)")
 ax.set_yscale('log')
 fig.savefig('eight_delay_comparison.png')
 #plt.show()
