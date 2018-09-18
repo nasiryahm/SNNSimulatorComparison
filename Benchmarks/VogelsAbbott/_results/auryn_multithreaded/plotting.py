f4c0e77ce9b97368a930f39eb3157e97c40762ca
Nasir Ahmad <nasiryahm@gmail.com>
2018-09-18 16:19:33 +0100

    Addition of plotting and exp curve fitting for auryn multi-threaded speed

--- Benchmarks/VogelsAbbott/_results/auryn_multithreaded/plotting.py
+++ Benchmarks/VogelsAbbott/_results/auryn_multithreaded/plotting.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
 plt.style.use('ggplot')
 
 
 SpikeBenchmarkPath = "../timestep_8_delay/Spike.dat"
 with open(SpikeBenchmarkPath, 'r') as f:
-	SpikeBenchmark = f.read()
+        SpikeBenchmark = f.read()
+SpikeBenchmark = float(SpikeBenchmark) / 100.0
 
 
 AurynBenchmarks = []
@@ -13,18 +16,30 @@ AurynBenchmarkCores = [1, 2, 4, 8]
 for numCores in AurynBenchmarkCores:
     with open(str(numCores) + ".dat", 'r') as f:
         time = f.read()
-	AurynBenchmarks.append(float(time) / 100.0f)
+        AurynBenchmarks.append(float(time) / 100.0)
 
+print(AurynBenchmarkCores, AurynBenchmarks)
+
+# Fitting a line
+points = np.linspace(0, 8, 100)
+
+def expfunc(x, a, b, c):
+    return a * np.exp(-b *x) + c
+
+popt, pcov = curve_fit(expfunc, AurynBenchmarkCores, AurynBenchmarks)
+#plt.plot(points, expfunc(points, *popt))
 
 
 
 # Plotting single timestep results
 fig, ax = plt.subplots()
 ax.set_title("Multi-Threaded Vogels Abbot Benchmark Comparison")
-ax.scatter(numCores, AurynBenchmarks, color='k')
+ax.scatter(AurynBenchmarkCores, AurynBenchmarks, color='k', zorder=2, s=50)
+ax.plot([0, 8], [SpikeBenchmark, SpikeBenchmark], color='r', linewidth = 5, alpha=0.5, zorder=1)
+ax.plot(points, expfunc(points, *popt), color='b', linewidth = 5, alpha=0.5, zorder=1)
 ax.set_xlabel("Number of CPU Cores")
 ax.set_ylabel("Simulation Run Time (Normalized)")
-ax.set_ylim([10.0**-2, 10.0**2])
+ax.set_ylim([10.0**-2, 10.0**0])
 ax.set_yscale('log')
 fig.savefig('multithreaded_comparison.png')
 #plt.show()
